59f0eb3d90bd0b56a0efbef87ee1b824e01a53d3
Guillem96 <guillem.orellana@gmail.com>
2020-11-11 15:08:49 +0100

    Trade system sol

--- 3. trade-system/trade_system.py
+++ 3. trade-system/trade_system.py
@@ -37,7 +37,6 @@ shop = {
     }
 }
 
-
 # Ex 3 Menu ####################################################################
 
 def menu(options, title):
@@ -71,39 +70,72 @@ def buy_menu(player, shop):
         item = list(shop['stock'])[sel]
         if not buy(player, shop, item):
             print("You cannot buy " + item)
+        else:
+            print_player(player)
 
 
 def sell_menu(player, shop):
-    # TODO: Similar to buy menu but listing player's inventory items
-    pass
+    while True:
+        inventory_items = list(player['inventory'])
+
+        menu_entries = []
+        for item in inventory_items:
+            qty = player['inventory'][item]
+            menu_entries.append(f'{item.capitalize()} x{qty}')
+
+        sel = menu(menu_entries + ['Back'], title='What do you want to sell?')
+        if sel == len(inventory_items):
+            break # back
+        else:
+            if not sell(player, shop, inventory_items[sel]):
+                print("* You cannot sell a " + inventory_items[sel])
+
+            print_player(player)
+
 
 # Ex4 Buy Logic ################################################################
 
 def buy(player, shop, item_name):
-    # TODO: Implement this
-    if True:
+    if player['gold'] < shop['prices'][item_name]:
+        return False
+    elif shop['stock'][item_name] <= 0:
         return False
     else:
+        player['gold'] -= shop['prices'][item_name]
+
+        if item_name in player['inventory']:
+            player['inventory'][item_name] += 1
+        else:
+            player['inventory'][item_name] = 1
+
+        shop['stock'][item_name] -= 1
+
         return True
 
 
-# Ex5 Sell Logic ################################################################
+# Ex5 Sell Logic ###############################################################
 
 def sell(player, shop, item_name):
-    # TODO: Implement this
-    if True:
+    if player['inventory'][item_name] <= 0 or item_name not in shop['prices']:
         return False
     else:
+        player['gold'] += shop['prices'][item_name] // 2
+        player['inventory'][item_name] -= 1
+        shop['stock'][item_name] += 1
+        if player['inventory'][item_name] == 0:
+            del player['inventory'][item_name]
+
         return True
 
 
 def main():
+    print_player(player)
+
     while True:
         sel = menu(['Buy', 'Sell', 'Exit'], title='Shop Welcome')
         if sel == 0:
             buy_menu(player, shop)
         elif sel == 1:
-            # TODO: Implement sell menu
             sell_menu(player, shop)
         else:
             print('Goodbye!!')
